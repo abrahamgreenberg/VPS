f5f77caa86f7c7f9182b77fbe5dff836e292e089
abisammy <abrahamgreenberg123@gmail.com>
2025-11-02 16:10:42 +0100

    add deploy script

---
+++ peninei/scripts/deploy.sh
@@ -0,0 +1,21 @@
+
+echo "Where would you like to deploy?"
+echo "1) Live"
+echo "2) Staging (default)"
+read -p "Enter your choice [1-2]: " choice
+
+# Default to staging if no input is provided
+choice=${choice:-2}
+
+case $choice in
+    1)
+        ./deploy_live.sh
+        ;;
+    2)
+        ./deploy_staging.sh
+        ;;
+    *)
+        echo "Invalid choice. Exiting."
+        exit 1
+        ;;
+esac
