d8be0f57756acd529cbf2c9bed2f8bb289131c33
abisammy <abrahamgreenberg123@gmail.com>
2025-11-02 17:41:44 +0100

    enable watch

--- auth/auth/src/server.ts
+++ auth/auth/src/server.ts
@@ -4,14 +4,21 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 import dotenv from "dotenv";
 dotenv.config();
 
+const DEFAULT_URL = "http://testsite6001:80";
+
 const app = express();
 
 app.use(
   "/",
   createProxyMiddleware({
-    target: "http://testsite6001:6001",
+    target: DEFAULT_URL,
     changeOrigin: true,
     router: (req) => {
+      const host = req.headers.host;
+      const hostWithoutPort = host?.split(":")[0];
+      if (!host || !hostWithoutPort) return DEFAULT_URL;
+
+      console.log(`Received request for host: ${hostWithoutPort}`);
       const randInt = Math.floor(Math.random() * 3) + 1;
       console.log(`Routing request to: http://testsite600${randInt}:80`);
       return `http://testsite600${randInt}:80`;
